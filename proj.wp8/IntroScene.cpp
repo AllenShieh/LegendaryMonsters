e7ace27e382c8bf7e8125a1da6e01027602c1f73
gougoumemeda <a516550257@qq.com>
2015-11-01 22:01:50 +0800

    引导页增加7张

--- proj.wp8/IntroScene.cpp
+++ proj.wp8/IntroScene.cpp
@@ -12,16 +12,29 @@ bool IntroScene::init(){
 	if (!CCLayer::init()){    //初始化父类
 		return false;
 	}
-
+	char * pics[6] = { "storylead1.jpg", "storylead2.jpg", "storylead3.jpg", "storylead4.jpg", "storylead5.jpg", "storylead6.jpg" };
+	for (int i = 0; i < 6; ++i){
+		IntroScene::picSrc[i] = pics[i];
+	}
+	IntroScene::currPic = 0;
+	CCDirector::sharedDirector()->getTouchDispatcher()->addTargetedDelegate(this, 0, true);
 	//获取屏幕大小
 	CCSize visibleSize = CCDirector::sharedDirector()->getWinSize();
 
-	auto * IntroBackground = CCSprite::create("taopao.jpg");
-	CCSize pictureSize = IntroBackground->getContentSize();
-	IntroBackground->setScaleX(visibleSize.width/ pictureSize.width);
-	IntroBackground->setScaleY(visibleSize.height / pictureSize.height);
-	IntroBackground->setPosition(ccp(visibleSize.width / 2.0f, visibleSize.height / 2.0f));
-	this->addChild(IntroBackground);
+	IntroScene::IntroBackground1 = CCSprite::create(picSrc[0]);
+	CCSize pictureSize1 = IntroScene::IntroBackground1->getContentSize();
+	IntroScene::IntroBackground1->setScaleX(visibleSize.width / pictureSize1.width);
+	IntroScene::IntroBackground1->setScaleY(visibleSize.height / pictureSize1.height);
+	IntroScene::IntroBackground1->setPosition(ccp(visibleSize.width / 2.0f, visibleSize.height / 2.0f));
+	this->addChild(IntroScene::IntroBackground1);
+
+	IntroScene::IntroBackground2 = CCSprite::create(picSrc[1]);
+	CCSize pictureSize2 = IntroScene::IntroBackground2->getContentSize();
+	IntroScene::IntroBackground2->setScaleX(visibleSize.width / pictureSize2.width);
+	IntroScene::IntroBackground2->setScaleY(visibleSize.height / pictureSize2.height);
+	IntroScene::IntroBackground2->setPosition(ccp(visibleSize.width / 2.0f, visibleSize.height / 2.0f));
+	this->addChild(IntroScene::IntroBackground2);
+	IntroScene::IntroBackground2->setVisible(false);
 
 	CCLabelTTF * skipLabel = CCLabelTTF::create("skip", "arial", 32);
 	skipLabel->setColor(ccc3(0, 0, 0));
@@ -32,6 +45,33 @@ bool IntroScene::init(){
 	menu->setPosition(CCPointZero);
 	this->addChild(menu);
 
+	setTouchEnabled(true);
+	return true;
+}
+
+bool IntroScene::ccTouchBegan(cocos2d::CCTouch *pTouch, cocos2d::CCEvent *pEvent){
+	IntroScene::IntroBackground2->setVisible(true);
+	if (IntroScene::currPic == 5){
+		auto menu = MenuScene::scene();
+		CCDirector::sharedDirector()->replaceScene(CCTransitionCrossFade::create(0.3f, menu));
+		IntroScene::currPic == 0;
+	}
+	else{
+		CCFadeOut * fadeout = CCFadeOut::create(1);
+		CCFadeIn * fadein = CCFadeIn::create(1);
+		IntroScene::currPic++;
+		CCTexture2D *newPic = CCTextureCache::sharedTextureCache()->addImage(IntroScene::picSrc[IntroScene::currPic]);
+		if (IntroScene::currPic % 2 == 0){
+			IntroScene::IntroBackground2->runAction(fadeout);
+			IntroScene::IntroBackground1->setTexture(newPic);
+			IntroScene::IntroBackground1->runAction(fadein);
+		}
+		else{
+			IntroScene::IntroBackground1->runAction(fadeout);
+			IntroScene::IntroBackground2->setTexture(newPic);
+			IntroScene::IntroBackground2->runAction(fadein);
+		}
+	}
 	return true;
 }
 
--- proj.wp8/IntroScene.h
+++ proj.wp8/IntroScene.h
@@ -8,12 +8,19 @@ USING_NS_CC;
 
 class IntroScene : public cocos2d::CCLayer
 {
+private:
+	CCSprite * IntroBackground1;
+	CCSprite * IntroBackground2;
+	char * picSrc[6];
+	int currPic;
 public:
 	static cocos2d::CCScene* scene();
 
 	virtual bool init();
 
 	void introSkipCall(CCObject* pSender);
+
+	bool ccTouchBegan(cocos2d::CCTouch *pTouch, cocos2d::CCEvent *pEvent);
 
 	CREATE_FUNC(IntroScene);
 };
